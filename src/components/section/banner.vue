2984ce575b874df71816c54facc5b881cd5886e9
u10210018 <u10210018@gmail.com>
2023-06-11 03:53:36 +0800

    update images url

--- src/components/section/banner.vue
+++ src/components/section/banner.vue
@@ -6,10 +6,10 @@ import MyImage from '~components/loader/image.vue';
     <div>
         <div>
             <MyImage
-                src="/images/banner.jpg"
-                src-webp="/images/banner.webp"
-                src-mobile="/images/banner-mobile.jpg"
-                src-mobile-webp="/images/banner-mobile.webp"
+                src="images/banner.jpg"
+                src-webp="images/banner.webp"
+                src-mobile="images/banner-mobile.jpg"
+                src-mobile-webp="images/banner-mobile.webp"
                 alt="logo"
                 class="h-auto w-full object-cover"
             />
